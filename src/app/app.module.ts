4d5d4c9efb171ee7149edf8c6d1e6e86f52618b4
wendel dos santos nunes <wendelnunes9999@gmail.com>
2024-08-24 08:52:07 -0300

    sparkles: Home and input page component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InputComponent } from './pages/home/components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
